2edb9497780270d52439b604a3cddc72c5cc80a5
Kim, Raymond <try2quit@korea.ac.kr>
2022-11-04 10:23:58 +0900

    Initial commit

---
+++ objDetectorTest/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  objDetectorTest
+//
+//  Created by raymond on 2022/11/04.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
